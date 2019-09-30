a21f2e5fb3cf87d29825d01a2979c04248a277ee
Jace Browning <jacebrowning@gmail.com>
2019-09-29 23:32:16 -0400

    Download 'geckodriver' automatically

--- twerk/utils.py
+++ twerk/utils.py
@@ -1,13 +1,22 @@
 import os
 from getpass import getpass
 
+import log
 from splinter import Browser
+from webdriver_manager.firefox import GeckoDriverManager
 
 from .models import Credentials
 
 
 def get_browser() -> Browser:
-    return Browser(wait_time=1.0)
+    try:
+        return Browser("firefox", wait_time=1.0)
+    except Exception as e:  # pylint: disable=broad-except
+        log.debug(str(e))
+        if "geckodriver" in str(e):
+            path = GeckoDriverManager().install()
+            return Browser("firefox", wait_time=1.0, executable_path=path)
+        raise e from None
 
 
 def get_credentials() -> Credentials:
