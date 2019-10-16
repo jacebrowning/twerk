8359e5e15153ea011ca1655f3078ee29c0a1fb98
Jace Browning <jacebrowning@gmail.com>
2019-10-15 22:32:21 -0400

    Add Chrome support

--- twerk/cli.py
+++ twerk/cli.py
@@ -20,11 +20,12 @@ def main():
     prompt="Twitter username",
     help="Username of Twitter account to automate.",
 )
+@click.option("--browser", help="Browser to use for automation.")
 @click.option("--debug", is_flag=True, help="Start debugger on exceptions.")
-def check(username: str, debug: bool):
-    with get_browser() as browser:
+def check(username: str, browser: str, debug: bool):
+    with get_browser(browser) as b:
         try:
-            commands.check(browser, username, display=click.echo)
+            commands.check(b, username, display=click.echo)
         except Exception as e:  # pylint: disable=broad-except
             if debug:
                 import ipdb  # pylint: disable=import-outside-toplevel
@@ -38,9 +39,10 @@ def check(username: str, debug: bool):
 
 @main.command(help="Crawl for bots starting from seed account.")
 @click.argument("username", envvar="TWITTER_SEED_USERNAME")
-def crawl(username: str):
-    with get_browser() as browser:
-        commands.crawl(browser, username)
+@click.option("--browser", help="Browser to use for automation.")
+def crawl(username: str, browser: str):
+    with get_browser(browser) as b:
+        commands.crawl(b, username)
 
 
 @main.command(help="Verify browser automation is working.")
@@ -57,9 +59,10 @@ def crawl(username: str):
     hide_input=True,
     help="Password of twitter account to automate.",
 )
-def block(username: str, password: str):
-    with get_browser() as browser:
-        commands.block(browser, username, password, display=click.echo)
+@click.option("--browser", help="Browser to use for automation.")
+def block(username: str, password: str, browser: str):
+    with get_browser(browser) as b:
+        commands.block(b, username, password, display=click.echo)
 
 
 if __name__ == "__main__":  # pragma: no cover
